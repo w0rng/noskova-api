261b2b2c0f44a99be44e5a2241c62edbe0c8e283
w0rng <antonabramov2000@gmail.com>
2022-03-30 13:18:31 +0700

    add endpoint for get images

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from dzonsan import dzonsan
 
 app = Flask(__name__)
 
 
 @app.route("/dzonsan", methods=["GET"])
-def hello_world():
+def dzonsan_url():
     return dzonsan(request.json)
+
+
+@app.route("/image/<name>", methods=["GET"])
+def image(name):
+    return send_file(f"{name}.png")
